b05ab94fb1165f3b6a267755ab22ebfb05431742
fahma20 <fahmaxo@gmail.com>
2024-09-17 12:58:00 +0300

    functions parameter lab assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function introduction(Aki) {
+    return `Hi, my name is ${Aki}.`
+}
+
+function introductionWithLanguage(Aki, JavaScript) {
+    return `Hi, my name is ${Aki} and I am learning to program in ${JavaScript}.`
+}
+
+function introductionWithLanguageOptional (Aki, JavaScript = `JavaScript`) {
+    return `Hi, my name is ${Aki} and I am learning to program in ${JavaScript}.`
+}
